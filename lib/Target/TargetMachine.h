c0cbf6a2accfe93091d6754c3f50f9c9a840ba86
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-07-15 20:02:48 -0700

    Added Target Machine Definition for ConsensIS (Consenso Instruction Set)

---
+++ lib/Target/TargetMachine.h
@@ -0,0 +1,76 @@
+//===-- ConsensIS 
+//===-- ConsensISTargetMachine.h - Define TargetMachine for ConsensIS ---*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the ConsensIS specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CONSENSISTARGETMACHINE_H
+#define CONSENSISTARGETMACHINE_H
+
+#include "ConsensISInstrInfo.h"
+#include "ConsensISISelLowering.h"
+#include "ConsensISFrameLowering.h"
+#include "ConsensISSelectionDAGInfo.h"
+#include "ConsensISSubtarget.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetData.h"
+#include "llvm/Target/TargetFrameLowering.h"
+
+namespace llvm {
+
+class ConsensISTargetMachine : public LLVMTargetMachine {
+  ConsensISSubtarget Subtarget;
+  const TargetData DataLayout;       // Calculates type size & alignment
+  ConsensISInstrInfo InstrInfo;
+  ConsensISTargetLowering TLInfo;
+  ConsensISSelectionDAGInfo TSInfo;
+  ConsensISFrameLowering FrameLowering;
+public:
+  ConsensISTargetMachine(const Target &T, StringRef TT,
+                     StringRef CPU, StringRef FS, const TargetOptions &Options,
+                     Reloc::Model RM, CodeModel::Model CM,
+                     CodeGenOpt::Level OL, bool is64bit);
+
+  virtual const ConsensISInstrInfo *getInstrInfo() const { return &InstrInfo; }
+  virtual const TargetFrameLowering  *getFrameLowering() const {
+    return &FrameLowering;
+  }
+  virtual const ConsensISSubtarget   *getSubtargetImpl() const{ return &Subtarget; }
+  virtual const ConsensISRegisterInfo *getRegisterInfo() const {
+    return &InstrInfo.getRegisterInfo();
+  }
+  virtual const ConsensISTargetLowering* getTargetLowering() const {
+    return &TLInfo;
+  }
+  virtual const ConsensISSelectionDAGInfo* getSelectionDAGInfo() const {
+    return &TSInfo;
+  }
+  virtual const TargetData       *getTargetData() const { return &DataLayout; }
+
+  // Pass Pipeline Configuration
+  virtual TargetPassConfig *createPassConfig(PassManagerBase &PM);
+};
+
+/// ConsensISV8TargetMachine - ConsensIS 32-bit target machine
+///
+class ConsensISV8TargetMachine : public ConsensISTargetMachine {
+  virtual void anchor();
+public:
+  ConsensISV8TargetMachine(const Target &T, StringRef TT,
+                       StringRef CPU, StringRef FS,
+                       const TargetOptions &Options,
+                       Reloc::Model RM, CodeModel::Model CM,
+                       CodeGenOpt::Level OL);
+};
+
+} // end namespace llvm
+
+#endif
